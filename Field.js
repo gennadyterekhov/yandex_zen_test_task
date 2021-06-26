d3666398ca38f2887826484a55b275d3817ea512
Gennady Terekhov <ter.ge@yandex.ru>
2021-06-26 16:11:42 +0300

    init

---
+++ Field.js
@@ -0,0 +1,94 @@
+export class Field {
+    constructor(points, config) {
+        this.config = config;
+        this.points = points;
+        this.height = points.length;
+        this.width = points[0].length;
+        this.aliveStr = this.config.alive_string;
+        this.deadStr = this.config.dead_string;
+        this.timeout = this.config.timeout;
+    }
+
+    show() {
+        for (let y = 0; y < this.height; y += 1) {
+            let line = '';
+            for (let x = 0; x < this.width; x += 1) {
+                line += (this.points[y][x] === 1) ? this.aliveStr : this.deadStr;
+            }
+            console.log(line);
+        }
+    }
+
+    showNeighboursMatrix() {
+        for (let y = 0; y < this.height; y += 1) {
+            let line = '';
+            for (let x = 0; x < this.width; x += 1) {
+                line += `${this.neighboursMatrix[y][x]} `;
+            }
+            console.log(line);
+        }
+    }
+
+    play() {
+        let iteration = 0;
+        let prePreviousState = [];
+        let refreshId = setInterval(() => {
+            console.log(`\n============= ITERATION ${iteration} =============\n`);
+            iteration += 1;
+            if (iteration % 2 === 0) {
+                prePreviousState = JSON.parse(JSON.stringify(this.points));
+            }
+            this.show();
+            this.points = this.getMutatedPoints();
+
+            if (JSON.stringify(prePreviousState) == JSON.stringify(this.points)) {
+                console.log('Game field reached stasis or entered loop');
+                clearInterval(refreshId);
+            }
+        }, this.timeout);
+    }
+
+    countAliveNeighbours(x, y) {
+        let aliveCount = 0;
+        const neighboursCoordinates = this.getNeighboursCoordinates(x, y);
+        for (let i = 0; i < neighboursCoordinates.length; i += 1) {
+            aliveCount += this.points[neighboursCoordinates[i][1]][neighboursCoordinates[i][0]];
+        }
+        return aliveCount;
+    }
+
+    getNeighboursCoordinates(targetX, targetY) {
+        return [
+            [targetX - 1, targetY],
+            [targetX + 1, targetY],
+            [targetX, targetY - 1],
+            [targetX, targetY + 1],
+            [targetX - 1, targetY - 1],
+            [targetX - 1, targetY + 1],
+            [targetX + 1, targetY - 1],
+            [targetX + 1, targetY + 1]
+        ].filter((value) => {
+            const x = value[0];
+            const y = value[1];
+            const xOk = ((x >= 0) && (x < this.width));
+            const yOk = ((y >= 0) && (y < this.height));
+            return (xOk && yOk);
+        });
+    }
+
+    getMutatedPoints() {
+        let newPoints = JSON.parse(JSON.stringify(this.points));
+        for (let y = 0; y < this.height; y += 1) {
+            for (let x = 0; x < this.width; x += 1) {
+                newPoints[y][x] = this.getMutatedPoint(x, y);
+            }
+        }
+        return newPoints;
+    }
+
+    getMutatedPoint(x, y) {
+        const aliveNeighbours = this.countAliveNeighbours(x, y);
+        const alive = (this.points[y][x] === 1) ? true : false;
+        return ((alive && (aliveNeighbours === 2)) || (aliveNeighbours === 3)) ? 1 : 0;
+    }
+}
---
+++ script.js
@@ -0,0 +1,104 @@
+import { Field } from './Field.js';
+import * as fs from 'fs';
+
+
+function getDataFromFile(filename) {
+    let data;
+    try {
+        data = fs.readFileSync(filename, 'utf8');
+    } catch (err) {
+        console.error(err);
+        process.exit();
+    }
+    return data;
+}
+
+function getDataFromCsvConfig(filename) {
+    let points = getDataFromFile(filename);
+    try {
+        points = points.split('\n')
+            .filter((value) => value)
+            .map((value) => {
+                    return value.split(',').map((value) => parseInt(value));
+                }
+            );
+    } catch (err) {
+        console.error(err);
+        process.exit();
+    }
+    return points;
+}
+
+function getDataFromJsonConfig(filename) {
+    let config = getDataFromFile(filename);;
+    try {
+        config = JSON.parse(fs.readFileSync(filename, 'utf8'));
+    } catch (err) {
+        console.error(err);
+        process.exit();
+    }
+    return config;
+}
+
+function getRandomPoints(width, height) {
+    let randomPoints = [];
+    for (let y = 0; y < height; y += 1) {
+        randomPoints[y] = getRandomArray(width);
+    }
+    return randomPoints;
+}
+
+function getRandomArray(length) {
+    let randomArray = new Array(length);
+    for (let i = 0; i < length; i += 1) {
+        randomArray[i] = Math.floor(Math.random() * 2);
+    }
+    return randomArray;
+}
+
+function getRandomWidth(appConfig) {
+    return appConfig.min_width + Math.floor(Math.random() * (appConfig.max_width - appConfig.min_width));
+}
+
+function getRandomHeight(appConfig) {
+    return appConfig.min_height + Math.floor(Math.random() * (appConfig.max_height - appConfig.min_height));
+}
+
+function main() {
+    if (process.argv.length < 3) {
+        // console.error('usage: node script.js <random [width] [height]|csv_field_filename> [--config=application_config]');
+        console.error('usage: node script.js <random [width] [height]|csv_field_filename>');
+        process.exit();
+    }
+    const initialStateType = process.argv[2];
+    
+    console.log(`Game field initial state provider is ${initialStateType}`);
+
+    const appConfig = getDataFromJsonConfig('config.json');
+
+    let points = [];
+    if (initialStateType === 'random') {
+        const width = process.argv[3] || getRandomWidth(appConfig);
+        const height = process.argv[4] || getRandomHeight(appConfig);
+        console.log(`Width ${width}`);
+        console.log(`Height ${height}`);
+        points = getRandomPoints(width, height);
+    } else {
+        try {
+            if (fs.existsSync(initialStateType)) {
+                points = getDataFromCsvConfig(initialStateType);
+            } else {
+                console.error('csv field file not found');
+                process.exit();
+            }
+        } catch (err) {
+            console.error(err);
+            process.exit();
+        }
+    }
+
+    const field = new Field(points, appConfig);
+    field.play();
+}
+
+main();
